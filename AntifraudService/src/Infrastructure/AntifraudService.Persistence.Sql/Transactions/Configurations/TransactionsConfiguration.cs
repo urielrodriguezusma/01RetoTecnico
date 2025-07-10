eb53cd5d8c06acb6633e235c40e877d2a1a87a32
Uriel Rodriguez Usma <urodriguez@ext.laligatech.com>
2025-07-10 16:59:30 -0500

    Adding converter

--- AntifraudService/src/Infrastructure/AntifraudService.Persistence.Sql/Transactions/Configurations/TransactionsConfiguration.cs
+++ AntifraudService/src/Infrastructure/AntifraudService.Persistence.Sql/Transactions/Configurations/TransactionsConfiguration.cs
@@ -1,6 +1,8 @@
 ﻿using AntifraudService.Domain.Transaction.Entities;
+using AntifraudService.Domain.Transaction.Enums;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AntifraudService.Persistence.Sql.Transactions.Configurations;
 public class TransactionsConfiguration : IEntityTypeConfiguration<Transaction>
@@ -10,5 +12,13 @@ public class TransactionsConfiguration : IEntityTypeConfiguration<Transaction>
         builder.ToTable("Transactions");
         builder.HasKey(t => t.TransferId);
         builder.Property(t => t.TransferId).ValueGeneratedNever();
+
+        var statusConverter = new ValueConverter<TransactionStatus, string>(
+            v => v.ToString(),
+            v => (TransactionStatus)Enum.Parse(typeof(TransactionStatus), v));
+
+        builder.Property(t => t.Status)
+            .IsRequired()
+            .HasConversion(statusConverter);
     }
 }
